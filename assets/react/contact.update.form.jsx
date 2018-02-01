a92680f4ebb0ef1a5072971caaa306a804f18bd4
Joaquin Astelarra <joaquinastelarra@gmail.com>
2018-02-01 14:30:49 +0100

    defined updateContact component skeleton. Work in progress.

---
+++ assets/react/contact.update.form.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class UpdateContact extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state = this.defaultState();
+
+        //TODO implement and bind handler
+    }
+
+    handleChange(event){
+
+    }
+
+    handleSubmit(event){
+
+    }
+
+    defaultState(){
+        return {
+
+        };
+    }
+
+    render(){
+        return(
+          <form></form>
+        );
+    }
+}
